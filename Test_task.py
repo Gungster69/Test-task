9870fca068bfa05b37a942091a9312d220c0a425
Gungster69 <157902040+Gungster69@users.noreply.github.com>
2024-02-20 12:23:17 +0700

    Working_code

--- Test_task.py
+++ Test_task.py
@@ -1,5 +1,7 @@
 import time
 
+import requests
+from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -13,47 +15,33 @@ class URLS:
     url = "https://koshelek.ru/authorization/signup"
 
 
-def shadow_root_reader():  #
-    driver = webdriver.Chrome()
-    driver.get(URLS.url)
-    shadow_host = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//div[@class = 'remoteComponent']"))
-    )
-    shadow_root = shadow_host.shadow_root
+driver = webdriver.Chrome()
+driver.get(URLS.url)
+shadow_host = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "div[class = 'remoteComponent']"))
+)
+read = shadow_host.shadow_root
 
-    return shadow_root
+CUT_HTML = read.find_element(By.CSS_SELECTOR, "div[data-wi='referral']")
 
 
-read = shadow_root_reader()
-
-we = read.find_element(By.CSS_SELECTOR, "label[inertText = 'Реферальный код']")
-# parent = we.findElement(By.XPATH(".."))
+# referral_loc = read.find_element(By.CSS_SELECTOR, '')
+# ref = referral_loc.shadow_root
 
 
 class Locators:  # Класс со всеми найденными локаторами для поиска окон ввода
-    LOCATOR_USERNAME = read.find_element(By.CSS_SELECTOR, "input[id^= 'input-']")
+    LOCATOR_USERNAME = read.find_element(By.CSS_SELECTOR, "input[id*= 'input-']")
     LOCATOR_EMAIL = read.find_element(By.CSS_SELECTOR, "input[id = 'username']")
     LOCATOR_PASSWORD = read.find_element(By.CSS_SELECTOR, "input[id = 'new-password']")
-    LOCATOR_REFERAL = read.find_element(
-        By.CSS_SELECTOR, "label[inertText = 'Реферальный код']"
-    )
+    LOCATOR_REFERRAL = CUT_HTML.find_element(By.CSS_SELECTOR, "input[id*='input-']")
 
 
-# Locators.LOCATOR_USERNAME.send_keys("Юрий")
-# Locators.LOCATOR_EMAIL.send_keys("Androsov.2002@mail.ru")
-# Locators.LOCATOR_PASSWORD.send_keys("12345678")
-we.send_keys("11110000")
+Locators.LOCATOR_USERNAME.send_keys("Юрий")
+Locators.LOCATOR_EMAIL.send_keys("Androsov.2002@mail.ru")
+Locators.LOCATOR_PASSWORD.send_keys("12345678")
+Locators.LOCATOR_REFERRAL.send_keys("11110000")
 
 
-class auto_test:
-
-    def check_len_name_less_then_6(self, name_to_check):
-        input_name = self.Locators.LOCATOR_USERNAME.send_keys(name_to_check)
-        assert len(name_to_check) > 6
-
-    def check_len_name_more_then_32(self, name_to_check):
-        input_name = self.Locators.LOCATOR_USERNAME.send_keys(name_to_check)
-        assert len(name_to_check) < 32
-
-
+# b = Locators.LOCATOR_USERNAME.name
+# print(b)
 a = input()
